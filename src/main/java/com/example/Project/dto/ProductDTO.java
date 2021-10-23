b4732606859a5ce1ef9ff7344291294b94acadda
Assylay <test1@mail.ru>
2021-10-23 15:47:46 +0600

    Initial commit

---
+++ src/main/java/com/example/Project/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.example.Project.dto;
+
+
+import lombok.Data;
+
+
+
+@Data
+public class ProductDTO {
+    private long id;
+    private String name;
+    private int categoryId;
+    private double price;
+    private double weight;
+    private String description;
+    private String imageName;
+
+}
